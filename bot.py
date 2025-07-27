7893d95f2adc5799435636135b21a49a61093fc8
Pavel Khmara <375295560023pk@gmail.com>
2025-07-27 10:09:39 +0200

    gitIgnore, env example

--- bot.py
+++ bot.py
@@ -1,10 +1,16 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
 from telegram.ext import Application, CommandHandler
 
 async def start(update, context):
     await update.message.reply_text('Hello world!')
 
 def main():
-    app = Application.builder().token('TOKEN').build()
+    app = Application.builder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler('start', start))
     app.run_polling()
 
