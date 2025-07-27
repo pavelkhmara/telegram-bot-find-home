309e67f92f43c5133c632e8f86b76806c88148bf
Pavel Khmara <375295560023pk@gmail.com>
2025-07-27 13:34:32 +0200

    Fix asyncio.new_event_loop()

--- bot.py
+++ bot.py
@@ -54,6 +54,6 @@ async def main():
 
 
 if __name__ == '__main__':
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    loop.run_until_complete(main())
+    import asyncio
+    asyncio.run(main())
+
