ef3acec60864fead15c3683b213d2c13b88ab30a
Pavel Khmara <375295560023pk@gmail.com>
2025-07-27 13:41:31 +0200

    Fix python-telegram-bot lib error

--- bot.py
+++ bot.py
@@ -40,8 +40,8 @@ async def show_filter(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Текущий фильтр: \n" + "\n".join(lines))
 
 
-def main():
-    asyncio.run(init_db())
+async def main():
+    await init_db()
 
     app = Application.builder().token(BOT_TOKEN).build()
 
@@ -51,8 +51,8 @@ def main():
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_menu_selection))
 
     print("✅ Бот запущен")
-    app.run_polling()  # НЕ await
+    await app.run_polling()
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
